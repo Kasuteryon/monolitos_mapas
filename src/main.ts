21ca406c9ab8b66a42233f9462cd3ecd60668604
Kasuteryon <eduardocastellon2002@gmail.com>
2025-10-13 01:51:32 -0600

    Nueva galerya y GA

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    { provide: BrowserAnimationsModule }
+    { provide: BrowserAnimationsModule }, provideFirebaseApp(() => initializeApp({ projectId: "desmontes-mapa", appId: "1:391187962473:web:74e1adc7d81dd6c35420f8", storageBucket: "desmontes-mapa.firebasestorage.app", apiKey: "AIzaSyCwgpEEP15Zr1JyVfdpepi0w6Nc7yDurMc", authDomain: "desmontes-mapa.firebaseapp.com", messagingSenderId: "391187962473", measurementId: "G-JKHM71PXXQ" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService
   ],
 });
